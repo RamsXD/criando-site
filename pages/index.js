4ead868f3f037649de46cf164bf22d28886eadfc
RamsXD <ramsxd67@gmail.com>
2025-10-20 12:46:39 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Home test</h1>
 
       <Link href="/sobre">Acessar página Sobre</Link>
     </div>
